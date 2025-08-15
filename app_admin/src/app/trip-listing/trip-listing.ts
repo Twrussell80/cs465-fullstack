4b46acd3c669aa91da9a316270d9476abd2bb233
Tim Russell <timothy.russell2@snhu.edu>
2025-08-15 17:55:44 -0500

    Module 6 Completed Baseline

---
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trips } from '../data/trips';
+import { TripCard } from '../trip-card/trip-card';
+import { Trip } from '../models/trip';
+import { TripData } from '../services/trip-data';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-trip-listing',
+  imports: [CommonModule, TripCard],
+  templateUrl: './trip-listing.html',
+  styleUrl: './trip-listing.css',
+  providers: [TripData]
+
+})
+
+
+export class TripListingComponent implements OnInit {
+
+  trips!: Trip[];
+  message: string = '';
+
+  constructor(
+    private tripDataService: TripData,
+    private router: Router) {
+    console.log('trip-listing constructor');
+  }
+
+  public addTrip(): void {
+    this.router.navigate(['add-trip']);
+  }
+
+  private getStuff(): void {
+    this.tripDataService.getTrips()
+      .subscribe({
+        next: (value: any) => {
+          this.trips = value;
+          if(value.length > 0)
+          {
+            this.message = 'There are ' + value.length + ' trips available.';
+          }
+          console.log(this.message);
+        },
+        error: (error: any) => {
+          console.log('Error: ' + error);
+        }
+      })
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.getStuff();
+  }
+
+}
